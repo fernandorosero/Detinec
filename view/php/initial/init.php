ea2b2e0c99211a52723ddd6ed8e53da62228a63d
xe51781 <xe51781@W19620DD.bbva.igrupobbva>
2016-09-27 13:16:58 +0200

    Curr

--- view/php/initial/init.php
+++ view/php/initial/init.php
@@ -1,6 +1,12 @@
-<?php
-?>
-
+    <! -- Imágenes, Íconos -->
     <link rel="icon" href="view/ico/logo_xl.ico"/>
-    <link rel="stylesheet" type="text/css" href="view/css/custom.css"
+    <!-- Estilos -->
+    <link rel="stylesheet" type="text/css" href="view/css/custom.css"/>
+    
+    <!-- Constante y Utilidades -->
+    <?php
+        include_once '/utils/DetinecConstantes.php';
+        include_once '/utils/DetinecUtil.php';
+    ?>
+    
 
