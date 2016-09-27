71369b948f18154cae3f89213da314c0a1d74825
xe51781 <xe51781@W19620DD.bbva.igrupobbva>
2016-09-27 14:03:09 +0200

    Curr

--- view/php/body/nav.php
+++ view/php/body/nav.php
@@ -4,7 +4,8 @@
         <ul>
             <li><a href="#">Inicio</a></li>
             <li><a href="#">Clientes</a></li>
-            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Acerca de</a></li>
+            <li><a href="#">Contacto</a></li>   
         </ul>
     </div>
 
---
+++ view/php/pages/loggin.php
@@ -0,0 +1,13 @@
+<?php
+?>
+
+    
+        <form>
+            <label>Usuario:</label>
+            <input type="text"/>
+            <label>Contraseña:</label>
+            <input type="text"/>
+            <button type="submit">Entrar</button>
+        </form>
+       
+    
