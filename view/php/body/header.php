62849d21eacbddc2bb1ad3691eb31f4de31765ff
xe51781 <xe51781@W19620DD.bbva.igrupobbva>
2016-09-27 14:01:53 +0200

    Curr

--- view/php/body/header.php
+++ view/php/body/header.php
@@ -3,4 +3,5 @@
     <div>
         <img src="view/img/logo_xl.png" alt="Logo DeTinEc"/>
         <p>Departamento Tecnológico Industrial</p>
+        <a href="view/php/pages/loggin.php">Loggin</a>
     </div>
