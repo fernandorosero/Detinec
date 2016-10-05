f8d71fc90c549956a50998e16589f120d335978b
xe51781 <xe51781@W19620DD.bbva.igrupobbva>
2016-10-05 11:31:49 +0200

    Curr

---
+++ utils/CookieDetinecPolitica.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<p>Utilizamos cookies propias y de terceros para mejorar nuestros servicios y mostrarle publicidad relacionada con sus preferencias mediante el análisis de sus hábitos de navegación. Si continua navegando, consideramos que acepta su uso. 
+Para obtener más información, o bien conocer cómo cambiar la configuración, consulte nuestra política de cookies.</p>
+<button>Aceptar</button>
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
