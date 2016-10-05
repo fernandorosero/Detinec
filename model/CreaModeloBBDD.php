032b08d95bd005303f33998e84dc80b34b495300
xe51781 <xe51781@W19620DD.bbva.igrupobbva>
2016-10-05 19:10:36 +0200

    Curr

---
+++ model/CreaModeloBBDD.php
@@ -0,0 +1,21 @@
+<?php
+
+    include_once '../utils/DetinecConstantes.php';
+    $slq = 'create database '.$db_name;
+    $conexion_crearModelo = mysql_connect($url, $user, $password);
+    if(mysql_query($slq, $conexion_crearModelo)){
+        echo 'La base de datos '.$db_name.' se cre&oacute; correctamente. ';
+        
+    }
+    else{
+        echo 'Error al crear la base de datos: '. mysql_error().". ";
+    }
+
+    $crear_tabla = @mysql_db_query($db_name,$tabla_contactos, $conexion_crearModelo) or die(mysql_error());
+    if($crear_tabla){
+        echo 'La tabla contactos se ha creado!. ';
+    }else
+    {
+        echo 'No se ha creado la tabla contactos. ';
+    }
+?>
--- utils/DetinecConstantes.php
+++ utils/DetinecConstantes.php
@@ -1,5 +1,23 @@
 <?php
 
+    $db_name="detinec";
+    
+    $tabla_contactos="CREATE TABLE IF NOT EXISTS contactos "
+            . "(id_contacto integer unsigned NOT NULL auto_increment, "
+            . "nombre varchar(30) NOT NULL, "
+            . "telefono int(11) NOT NULL, "
+            . "PRIMARY KEY (id_contacto));";
+
+    /*Local*/
+    $url="localhost";
+    $user="root";
+    $password="";
+    
+    /*Produccion
+    $url="localhost";
+    $user="";
+    $password="";
+    */
 ?>
 
 
