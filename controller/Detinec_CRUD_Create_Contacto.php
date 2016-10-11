e09f9d067bc614600f099f5b1132782dac96f29d
xe51781 <xe51781@W19620DD.bbva.igrupobbva>
2016-10-11 17:18:35 +0200

    Curr

--- controller/Detinec_CRUD_Create_Contacto.php
+++ controller/Detinec_CRUD_Create_Contacto.php
@@ -3,9 +3,15 @@
     $telefono = $_POST['telefono'];
     $email = $_POST['email'];
     $comentario = $_POST['comentario'];
-    
+    include_once '../model/AbreConexionBBDD.php';
     $resultado_deti = $nombre.' '.$telefono.' '.$email.' '.$comentario;
 
+    $query_create = "insert into contactos(nombre, telefono, email, comentario)"
+            . " values('$nombre', '$telefono', '$email', '$comentario');";
+    
+    $query = mysql_query($query_create, $conexion_db)
+            or die("NO SE HA PODIDO CREAR");
+    include_once '../model/CierraConexionBBDD.php';
     echo $resultado_deti;
 ?>
 
