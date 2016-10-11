dd107b00677f39f900670ff0ad873be9a622ebd0
xe51781 <xe51781@W19620DD.bbva.igrupobbva>
2016-10-11 13:01:44 +0200

    Curr

--- utils/DetinecConstantes.php
+++ utils/DetinecConstantes.php
@@ -3,13 +3,13 @@
     $db_name="detinec";
     
     $tabla_contactos="CREATE TABLE IF NOT EXISTS contactos "
-            . "(id_contacto integer unsigned not null auto_increment, "
-            . "nombre varchar(30) not null, "
-            . "telefono int(11) not null, "
-            . "correo varchar (30) not null, "
-            . "mensaje varchar(200) not null, "
+            . "(id_contacto integer unsigned NOT NULL auto_increment, "
+            . "nombre varchar(30) NOT NULL, "
+            . "telefono int(11) NOT NULL, "
+            . "email varchar(50) NOT NULL, "
+            . "comentario varchar(120) NOT NULL, "
             . "PRIMARY KEY (id_contacto));";
-
+    
     /*Local*/
     $url="localhost";
     $user="root";
