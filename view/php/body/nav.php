0bbb602edfa05b3471e3a23f01825e0aa6c7da8a
xe51781 <xe51781@W19620DD.bbva.igrupobbva>
2016-10-17 13:45:44 +0200

    Curr

--- view/php/body/nav.php
+++ view/php/body/nav.php
@@ -1,11 +1,23 @@
 <?php
+    $url_Adminstracion = "<li><a href='#'>Administraci&oacute;n</a></li>";
 ?>
     <div>
         <ul>
             <li><a href="index.php">Inicio</a></li>
             <li><a href="#">Clientes</a></li>
             <li><a href="#">Acerca de</a></li>
-            <li><a href="contacto.php">Contacto</a></li>   
+            <li><a href="contacto.php">Contacto</a></li>
+            <?php
+                if(isset($_POST['usuario'])){
+                    $_SESSION['usuario'] = $_POST['usuario'];
+                    echo $url_Adminstracion;
+                    
+                }else{
+                    if(isset($_SESSION['usuario'])){
+                        echo $url_Adminstracion;
+                    }
+            }
+            ?>
         </ul>
     </div>
 
