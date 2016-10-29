419a4db86d071287db2b897a109c0ed2430bf624
fernandorosero <tatofarp@hotmail.com>
2016-10-29 13:28:11 +0200

    Subida lenovo PHP

--- controller/ListadoContactosDetinec.php
+++ controller/ListadoContactosDetinec.php
@@ -5,11 +5,13 @@
      while($row = mysql_fetch_row($resultado))
      {
         echo "<tr>\n<td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td>\n<td>";
+            echo '<div id="formularioX">';
             echo"<form name='deleteContacto$row[0]' action='/Detinec/controller/BorrarContactoDetinec.php' method='post' "
                     . "onsubmit='return verificarBorrado()' >";
-                echo"<input type='text' name='idContactoDelete' value='".$row[0]."'/>";
+                echo"<input type='hidden' name='idContactoDelete' value='".$row[0]."'/>";
                 include '/controller/BotonBorrar.php';
             echo"</form>";
+            echo '</div>';
         echo"</td>\n</tr>";
      }
 include_once '/model/CierraConexionBBDD.php';
